e895f4137a51d3183dfdeac237261aa99c54198b
Eduardo Vinícius Delgado <eduardo.vini.delgado@gmail.com>
2022-09-08 18:29:14 -0300

    Correção nomes variáveis

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,18 +9,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm a").withZone(ZoneId.systemDefault());
-		Instant d01 = Instant.now();
-		LocalDateTime dtPortugal  = LocalDateTime.ofInstant(d01, ZoneId.of("Portugal"));
-		LocalDateTime dtJpan  = LocalDateTime.ofInstant(d01, ZoneId.of("Japan"));
-		LocalDateTime dtAustralia =  LocalDateTime.ofInstant(d01, ZoneId.of("Australia/Hobart"));
-		LocalDateTime dtGreenwich = LocalDateTime.ofInstant(d01, ZoneId.of("Greenwich"));
+		DateTimeFormatter formatoSaida = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm a").withZone(ZoneId.systemDefault());
+		Instant dtBrazil = Instant.now();
+		LocalDateTime dtPortugal  = LocalDateTime.ofInstant(dtBrazil, ZoneId.of("Portugal"));
+		LocalDateTime dtJapan  = LocalDateTime.ofInstant(dtBrazil, ZoneId.of("Japan"));
+		LocalDateTime dtAustralia =  LocalDateTime.ofInstant(dtBrazil, ZoneId.of("Australia/Hobart"));
+		LocalDateTime dtGreenwich = LocalDateTime.ofInstant(dtBrazil, ZoneId.of("Greenwich"));
 		
-		System.out.println("Dia e horário local (Brasil) - " + fmt1.format(d01));
-		System.out.println("Dia e horário remoto (Portugual) - " + dtPortugal.format(fmt1));
-		System.out.println("Dia e horário remoto (Japão) - " + dtJpan.format(fmt1));
-		System.out.println("Dia e horário remoto (Australia/Hobart) - " + dtAustralia.format(fmt1));
-		System.out.println("Dia e horário remoto (Londres) - " + dtGreenwich.format(fmt1));
+		System.out.println("Dia e horário local (Brasil) - " + formatoSaida.format(dtBrazil));
+		System.out.println("Dia e horário remoto (Portugual) - " + dtPortugal.format(formatoSaida));
+		System.out.println("Dia e horário remoto (Japão) - " + dtJapan.format(formatoSaida));
+		System.out.println("Dia e horário remoto (Australia/Hobart) - " + dtAustralia.format(formatoSaida));
+		System.out.println("Dia e horário remoto (Londres) - " + dtGreenwich.format(formatoSaida));
 	}
 
 }
